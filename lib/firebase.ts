c9dfc40d4fb42302a69e4eed481b70696815b2eb
oriana-monaldi <orimonaldi@gmail.com>
2025-11-18 16:22:55 -0300

    .

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -11,7 +11,6 @@ const firebaseConfig = {
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore(app);
 
