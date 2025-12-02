01e0669c909b5caa3ce9f35cfb19c573c976f567
oriana-monaldi <orimonaldi@gmail.com>
2025-12-02 19:04:33 -0300

    dsc

--- components/appointment-form.tsx
+++ components/appointment-form.tsx
@@ -257,7 +257,7 @@ export function AppointmentForm({
                         setFormData({ ...formData, fecha: e.target.value })
                       }
                       required
-                      className="h-12 px-4 bg-black/30 border-2 border-cyan-600/30 rounded-xl text-white focus:border-cyan-400 focus:ring-4 focus:ring-cyan-400/20 outline-none transition-all duration-300 group-hover:border-cyan-500/50"
+                      className="h-12 px-4 bg-black/30 border-2 border-cyan-600/30 rounded-xl text-white focus:border-cyan-400 focus:ring-4 focus:ring-cyan-400/20 outline-none transition-all duration-300 group-hover:border-cyan-500/50 [color-scheme:dark]"
                     />
                   </div>
                 </div>
@@ -276,7 +276,7 @@ export function AppointmentForm({
                         setFormData({ ...formData, hora: e.target.value })
                       }
                       required
-                      className=" h-12 px-4 bg-black/30 border-2 border-cyan-600/30 rounded-xl text-white focus:border-cyan-400 focus:ring-4 focus:ring-cyan-400/20 outline-none transition-all duration-300 group-hover:border-cyan-500/50"
+                      className="h-12 px-4 bg-black/30 border-2 border-cyan-600/30 rounded-xl text-white focus:border-cyan-400 focus:ring-4 focus:ring-cyan-400/20 outline-none transition-all duration-300 group-hover:border-cyan-500/50 [color-scheme:dark]"
                     />
                   </div>
                 </div>
